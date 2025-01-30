819e48d368de0cc987cf11345a76b89886a357dc
socar-foden <foden@socar.kr>
2025-01-31 08:57:46 +0900

    test

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const { promisify } = require('util');
-const { exec } = require('child_process');
+const { promisify } = require("util");
+const { exec } = require("child_process");
 
 const execPromise = promisify(exec);
 
 const execute = async (command) => {
-    try {
-      return await execPromise(command);
-    } catch (error) {
-      if (error.stdout === '') {
-        return { stdout: '', stderr: '' };
-      }
-      if (error.stderr) {
-        throw error.stderr;
-      }
+  try {
+    return await execPromise(command);
+  } catch (error) {
+    if (error.stdout === "") {
+      return { stdout: "", stderr: "" };
     }
-  };
+    if (error.stderr) {
+      throw error.stderr;
+    }
+  }
+};
 
 const test = async () => {
-    const { stdout } = await execute(`git branch -r --contains 1184cbbdeaee93994a40b3e2495a363c40821086`);
-    console.log(stdout)
-}
+  const { stdout } = await execute(
+    `git branch -r --contains 1184cbbdeaee93994a40b3e2495a363c40821086`
+  );
+  console.log(stdout);
+};
 
 test();
